df5ef08704e25f4a91c0e30fc0be30abba5aa10b
Matthieu Haefele <matthieu.haefele@univ-pau.fr>
2020-03-10 10:35:26 +0100

    adding srun tests

--- C/serial_parallel_hello/job_mpi.sh
+++ C/serial_parallel_hello/job_mpi.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=24
 #SBATCH -C miriel
 
-module load compiler/gcc/9.2.0 mpi/openmpi/4.0.2
+module load compiler/gcc/9.2.0 mpi/openmpi/4.0.2-testing
 work_directory="${SLURM_JOB_NAME}_${SLURM_JOB_ID}"
 mkdir -p "${work_directory}/staged"
 cd "${work_directory}"
@@ -15,9 +15,11 @@ ln ../post-process .
 
 echo "########### Launching simu ##############"
 mpirun ./simu_mpi
-#srun -N 2 --ntasks-per-node=24 ./simu_mpi
+#srun --mpi=openmpi -N 2 --ntasks-per-node=24 ./simu_mpi
 #srun --mpi=openmpi ./simu_mpi
 #srun --mpi=pmi2 ./simu_mpi
+#srun --mpi=pmix ./simu_mpi
+#srun --mpi=pmix_v3 ./simu_mpi
 
 echo "########### Launching post-process ##############"
 ./post-process 48
